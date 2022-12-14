09c4e8cb12992487185984fa7f03369ab1caef0d
chloe-chin <chloemchin@gmail.com>
2022-12-14 08:01:33 -0800

    Initial Commit

---
+++ side-project-chloe/side_project_chloeApp.swift
@@ -0,0 +1,17 @@
+//
+//  side_project_chloeApp.swift
+//  side-project-chloe
+//
+//  Created by chloe chin on 12/14/22.
+//
+
+import SwiftUI
+
+@main
+struct side_project_chloeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
